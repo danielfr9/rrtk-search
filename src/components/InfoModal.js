5e962471f8f13bf7dfd61511440e98d98ddb73ca
Daniel Fernandez <danielframos09@gmail.com>
2022-04-10 17:47:53 -0600

    add: API fetch to kanjiapi.dev when info modal is opened

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -1,9 +1,18 @@
-import { Modal, Badge } from "@mantine/core";
+// Data
 import noKanji from "../assets/noKanji";
+// Components
+import { Modal, Badge, Tooltip, ActionIcon } from "@mantine/core";
+// Hooks
+import { useClipboard } from "@mantine/hooks";
+// Icons
+import { BiCopy } from "react-icons/bi";
 
 const InfoModal = ({ content, opened, handleCloseModal }) => {
+  const clipboard = useClipboard({ timeout: 1500 });
+
   return (
     <Modal
+      classNames={{ title: "grow" }}
       overflow="inside"
       opened={opened}
       onClose={() => handleCloseModal()}
@@ -15,7 +24,7 @@ const InfoModal = ({ content, opened, handleCloseModal }) => {
         </div>
       }
     >
-      <div className="flex items-center justify-center py-4">
+      <div className="flex items-center justify-center pb-4">
         {content.kanji ? (
           <h2 className="text-5xl font-semibold py-5">{content.kanji}</h2>
         ) : (
@@ -28,6 +37,35 @@ const InfoModal = ({ content, opened, handleCloseModal }) => {
           </div>
         )}
       </div>
+      <div className="flex items-center justify-end">
+        {content.kanji && (
+          <Tooltip
+            gutter={10}
+            label={clipboard.copied ? "Copied" : "Copy Kanji"}
+            color={clipboard.copied ? "indigo" : "cyan"}
+            position="left"
+            withArrow
+          >
+            <ActionIcon
+              onClick={() => clipboard.copy(content.kanji)}
+              radius="sm"
+              variant="hover"
+              color="cyan"
+              size="lg"
+            >
+              <BiCopy size="1.5rem" />
+            </ActionIcon>
+          </Tooltip>
+        )}
+      </div>
+      <div className="flex pb-2">
+        <Badge className="mb-2 mr-2" color="indigo" size="md" variant="filled">
+          JLPT: {content.jlpt || "N/A"}
+        </Badge>
+        <Badge className="mb-2 mr-2" color="teal" size="md" variant="filled">
+          Grade: {content.grade || "N/A"}
+        </Badge>
+      </div>
       <div>
         <span className="text-lg font-semibold">
           {content.keywords.primary}
@@ -38,7 +76,31 @@ const InfoModal = ({ content, opened, handleCloseModal }) => {
           )}`}</span>
         )}
       </div>
-      <p className="text-md py-2">{content.description}</p>
+      <p className="text-md pt-2 pb-4">{content.description}</p>
+      {content.kanji && (
+        <>
+          <div className="flex pb-4">
+            <div style={{ width: 81 }} className="mr-2">
+              <Badge color="cyan" size="md" variant="filled">
+                Kunyomi:
+              </Badge>
+            </div>
+            <div>
+              <span>{content.kunyomi.join(", ")}</span>
+            </div>
+          </div>
+          <div className="flex pb-4">
+            <div style={{ width: 81 }} className="mr-2">
+              <Badge fullWidth color="red" size="md" variant="filled">
+                Onyomi:
+              </Badge>
+            </div>
+            <div>
+              <span>{content.onyomi.join(", ")}</span>
+            </div>
+          </div>
+        </>
+      )}
     </Modal>
   );
 };
--- src/hooks/useModal.js
+++ src/hooks/useModal.js
@@ -1,13 +1,31 @@
 import { useState } from "react";
 
-// TODO: API Fetch
-
 const useModal = () => {
   const [opened, setOpened] = useState(false);
   const [content, setContent] = useState(null);
 
   const handleOpenModal = async (data) => {
-    await setContent(data);
+    if (!data.kanji) {
+      await setContent(data);
+    } else {
+      try {
+        const result = await fetch(
+          `https://kanjiapi.dev/v1/kanji/${data.kanji}`
+        ).then((res) => res.json());
+
+        await setContent({
+          ...data,
+          onyomi: result.on_readings,
+          kunyomi: result.kun_readings,
+          stroke_count: result.stroke_count,
+          jlpt: result.jlpt,
+          grade: result.grade,
+        });
+      } catch {
+        console.log("Error Fetching Data");
+        await setContent(data);
+      }
+    }
     await setOpened(true);
   };
 
