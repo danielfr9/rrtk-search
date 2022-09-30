510f9d9b56a65768a09ef593974759e32788981e
Daniel <danielframos09@gmail.com>
2022-09-30 15:05:26 -0600

    add: no kanji found message

--- src/RRTK.tsx
+++ src/RRTK.tsx
@@ -12,6 +12,8 @@ import { VirtuosoGrid, VirtuosoGridHandle } from "react-virtuoso";
 import MainHeader from "./components/MainHeader";
 import KanjiCard from "./components/KanjiCard";
 import InfoModal from "./components/InfoModal";
+// Icons
+import { RiTranslate } from "react-icons/ri";
 
 const RRTK = () => {
   const virtuoso = useRef<VirtuosoGridHandle>(null);
@@ -33,21 +35,35 @@ const RRTK = () => {
           query={query}
           setQuery={setQuery}
         />
-        <VirtuosoGrid
-          ref={virtuoso}
-          style={{ flexGrow: 1 }}
-          totalCount={searchResult.length}
-          itemContent={(index) => (
-            <KanjiCard
-              data={searchResult[index]}
-              handleOpenModal={handleOpenModal}
-            />
-          )}
-          overscan={{ main: 200, reverse: 200 }}
-          listClassName={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-3 px-3 ${
-            dark && `bg-[#101113]`
-          }`}
-        />
+        {searchResult.length === 0 ? (
+          <div className="flex flex-col grow justify-center items-center m-4 space-y-4">
+            <div className="flex items-center space-x-6">
+              <RiTranslate className="w-8 h-8 md:w-12 md:h-12" />
+              <span className="font-semibold italic text-lg sm:text-xl md:text-2xl">
+                No Kanji Found!
+              </span>
+            </div>
+            <p className="text-xs sm:text-sm italic">
+              Please try with a different kanji, keyword or number.
+            </p>
+          </div>
+        ) : (
+          <VirtuosoGrid
+            ref={virtuoso}
+            style={{ flexGrow: 1 }}
+            totalCount={searchResult.length}
+            itemContent={(index) => (
+              <KanjiCard
+                data={searchResult[index]}
+                handleOpenModal={handleOpenModal}
+              />
+            )}
+            overscan={{ main: 200, reverse: 200 }}
+            listClassName={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-3 px-3 ${
+              dark && `bg-[#101113]`
+            }`}
+          />
+        )}
       </div>
       <InfoModal
         content={content}
